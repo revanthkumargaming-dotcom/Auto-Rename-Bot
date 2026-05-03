03ba953d0efbbdb9d19373241872a01530375601
revanthkumargaming-dotcom <revanthkumargaming@gmail.com>
2026-05-03 16:28:54 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,55 +1,63 @@
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
 from helper.database import madflixbotz
+
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+        return await message.reply_text(
+            "**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename}\n\n🔗 Size ➠ : {filesize}\n\n⏰ Duration ➠ : {duration}`**"
+        )
+
     caption = message.text.split(" ", 1)[1]
-    await bustersofcl.set_caption(message.from_user.id, caption=caption)
+    await madflixbotz.set_caption(message.from_user.id, caption=caption)
     await message.reply_text("**Your Caption Successfully Added ✅**")
-   
+
+
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message):
-    caption = await madflixbotz.get_caption(message.from_user.id)  
+    caption = await madflixbotz.get_caption(message.from_user.id)
+
     if not caption:
-       return await message.reply_text("**You Don't Have Any Caption ❌**")
+        return await message.reply_text("**You Don't Have Any Caption ❌**")
+
     await madflixbotz.set_caption(message.from_user.id, caption=None)
     await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
-                                       
+
+
 @Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
 async def see_caption(client, message):
-    caption = await madflixbotz.get_caption(message.from_user.id)  
+    caption = await madflixbotz.get_caption(message.from_user.id)
+
     if caption:
-       await message.reply_text(f"**Your Caption :**\n\n`{caption}`")
+        await message.reply_text(f"**Your Caption :**\n\n`{caption}`")
     else:
-       await message.reply_text("**You Don't Have Any Caption ❌**")
+        await message.reply_text("**You Don't Have Any Caption ❌**")
 
 
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
-async def viewthumb(client, message):    
+async def viewthumb(client, message):
     thumb = await madflixbotz.get_thumbnail(message.from_user.id)
+
     if thumb:
-       await client.send_photo(chat_id=message.chat.id, photo=thumb)
+        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("**You Don't Have Any Thumbnail ❌**") 
-		
+        await message.reply_text("**You Don't Have Any Thumbnail ❌**")
+
+
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await madflixbotz.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("**Thumbnail Deleted Successfully 🗑️**")
-	
+
+
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
-    await madflixbotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+
+    await madflixbotz.set_thumbnail(
+        message.from_user.id,
+        file_id=message.photo.file_id
+    )
+
     await mkn.edit("**Thumbnail Saved Successfully ✅️**")
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @busters_ofcl
-# Developer @Gojo
